8e9bff1edfc45413c5a071c0821539f661ffbc5e
Sebastian Godelet <sebastian.godelet@outlook.com>
2023-05-29 11:34:49 +1000

    Make LongToHex more efficient

--- GS.SkyWatcher/Commands.cs
+++ GS.SkyWatcher/Commands.cs
@@ -1777,18 +1777,20 @@ namespace GS.SkyWatcher
         /// Converts a long to Hex command
         /// </summary>
         /// <param name="number"></param>
-        /// <returns></returns>
+        /// <returns>31 -> 1F0000</returns>
         private static string LongToHex(long number)
         {
-            // 31 -> 0F0000
-            var a = ((int)number & 0xFF).ToString("X").ToUpper();
-            var b = (((int)number & 0xFF00) / 256).ToString("X").ToUpper();
-            var c = (((int)number & 0xFF0000) / 256 / 256).ToString("X").ToUpper();
+            var chars = new char[6];
 
-            if (a.Length == 1) { a = "0" + a; }
-            if (b.Length == 1) { b = "0" + b; }
-            if (c.Length == 1) { c = "0" + c; }
-            return a + b + c;
+            for (var i = 0; i < chars.Length; i += 2)
+            {
+                var part = ((int)number & 0xFF).ToString("X2");
+                chars[i] = part[0];
+                chars[i + 1] = part[1];
+                number >>= 8;
+            }
+
+            return new string(chars);
         }
 
         /// <summary>
