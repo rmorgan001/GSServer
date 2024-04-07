4596e925367d4689da9431b2f8e0113f90cc7052
Principia4834 <3509988+Principia4834@users.noreply.github.com>
2024-04-07 13:15:29 +0100

    Alt Az user interface enhancements
    Alt Az GoTo control implemented
    Az direction status indicator, toggles with SOP indicator
    Flip Az Direction button, toggles with Flip SOP button
    Fixes for Home and Park button functionality on floating buttons window
    Documentation updates for Alt Az user interface

--- GS.Server/SkyTelescope/SkyTelescopeV.xaml.cs
+++ GS.Server/SkyTelescope/SkyTelescopeV.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System.Runtime.InteropServices;
+using System.Windows;
 using System.Windows.Data;
+using System.Windows.Input;
 
 namespace GS.Server.SkyTelescope
 {
@@ -12,6 +14,22 @@ namespace GS.Server.SkyTelescope
         public SkyTelescopeV()
         {
             InitializeComponent();
+        }
+
+        private void GoToDialog_OnMouseRightButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            var name = ((FrameworkElement)sender).Name;
+            switch (name)
+            {
+                case "RaDecGoTo":
+                    SkyTelescopeVM._skyTelescopeVM.RaDecDialogActive = false;
+                    SkyTelescopeVM._skyTelescopeVM.AltAzDialogActive = true;
+                    break;
+                case "AltAzGoTo":
+                    SkyTelescopeVM._skyTelescopeVM.RaDecDialogActive = true;
+                    SkyTelescopeVM._skyTelescopeVM.AltAzDialogActive = false;
+                    break;
+            }
         }
     }
 }
