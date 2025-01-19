4421e51548447ad5c5e799b594bb4310e4861c21
rmorgan001 <robert.morgan.e@gmail.com>
2025-01-19 14:57:13 -0500

    Update Copyright info for GS.Server project

--- GS.Server/SkyTelescope/SkyPredictor.cs
+++ GS.Server/SkyTelescope/SkyPredictor.cs
@@ -1,4 +1,4 @@
-﻿/* Copyright(C) 2019-2024 Rob Morgan (robert.morgan.e@gmail.com)
+﻿/* Copyright(C) 2019-2025 Rob Morgan (robert.morgan.e@gmail.com)
    Copyright(C) 2024 Andy Watson
 
     This program is free software: you can redistribute it and/or modify
